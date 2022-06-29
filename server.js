ad5253f40e2c8eafb82bc3c7143f2054ac6edbeb
urieskenasy <urieskenasy@gmail.com>
2022-06-29 16:07:20 +0200

    init

--- server.js
+++ server.js
@@ -1,3 +1,40 @@
+const { initializeApp } = require("firebase/app");
+const {
+    getFirestore,
+    collection,
+    addDoc,
+    getDoc,
+    updateDoc,
+    doc,
+} = require("firebase/firestore");
+const { getAuth } = require("firebase/auth");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAibWkuGjiJ5ngOcKfThSuBLWb4ZH4Ph3Y",
+
+    authDomain: "hobbyt-6b5c6.firebaseapp.com",
+
+    projectId: "hobbyt-6b5c6",
+
+    storageBucket: "hobbyt-6b5c6.appspot.com",
+
+    messagingSenderId: "842001819775",
+
+    appId: "1:842001819775:web:a43733d4731f98d19e774a",
+
+    measurementId: "G-S80VK4QBKS",
+};
+
+// init firebase
+initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+const auth = getAuth();
+
+// collection ref
+const chatRef = collection(db, "chats");
+
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, { cors: { origin: "*" } });
@@ -8,21 +45,23 @@ http.listen(PORT, function () {
         const id = socket.handshake.query.id;
         socket.join(id);
 
-        socket.on("join_room", (data) => {
+        socket.on("join_room", async (data) => {
+            // const chatSnapShot = await getDoc(chatRef);
+            // if (chatSnapShot.exists()) {
+            //
+            // } else {
+            //     throw new Error("chat could not log");
+            // }
+            const docRef = await addDoc(chatRef, { ...data, messages: [] });
             socket.join(data);
-            console.log(`User with ID: ${socket.id} joined room: ${data}`);
+            console.log(data);
+            console.log(docRef);
         });
 
-        socket.on("send-message", ({ recipients, text }) => {
-            recipients.forEach((recipient) => {
-                const newRecipients = recipients.filter((r) => r !== recipient);
-                newRecipients.push(id);
-                socket.broadcast.to(recipient).emit("receive-message", {
-                    recipients: newRecipients,
-                    sender: id,
-                    text,
-                });
-            });
+        socket.on("send_message", async (data) => {
+            const message = doc(db, "chats", "4EGssmpdmr5IHuB1ro5h");
+            console.log(message);
+            const ref = await updateDoc(chatRef, {});
         });
     });
 });
